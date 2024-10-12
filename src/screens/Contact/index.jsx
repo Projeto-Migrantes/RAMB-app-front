eabef6eeb387d329d8fd42fe7e34ee7c08179ff4
Beatriz Siquara <beatrizsiquara@gmail.com>
2024-10-12 18:45:03 -0300

    atualizacao branch

--- src/screens/Contact/index.jsx
+++ src/screens/Contact/index.jsx
@@ -4,7 +4,7 @@ import theme from "@theme/index";
 import { View, Text } from "react-native";
 import { TitleWithIcon } from "@components/TitleWithIcon";
 import AntDesign from "@expo/vector-icons/AntDesign";
-import { Container, CommonInput, MessageInput} from "./styles";
+import { Container, CommonInput, MessageInput } from "./styles";
 import { useState } from "react";
 
 export function Contact() {
@@ -23,45 +23,44 @@ export function Contact() {
           <AntDesign name="contacts" size={26} color={theme.Colors.Purple} />
         }
       />
-      
-      <Container>
 
+      <Container>
         <Text>Nome</Text>
-          <CommonInput
-            value={name}
-            onChangeText={setName}
-            placeholder="Digite aqui"
-          />
+        <CommonInput
+          value={name}
+          onChangeText={setName}
+          placeholder="Digite aqui"
+        />
 
         <Text>Email</Text>
-          <CommonInput
-            value={email}
-            onChangeText={setEmail}
-            placeholder="Digite aqui"
-          />
+        <CommonInput
+          value={email}
+          onChangeText={setEmail}
+          placeholder="Digite aqui"
+        />
 
         <Text>Telefone</Text>
-          <CommonInput
-            value={phone}
-            onChangeText={setPhone}
-            placeholder="Digite aqui"
-          />
+        <CommonInput
+          value={phone}
+          onChangeText={setPhone}
+          placeholder="Digite aqui"
+        />
 
         <Text>Assunto</Text>
-          <CommonInput
-            value={subject}
-            onChangeText={setSubject}
-            placeholder="Digite aqui"
-          />
-          
+        <CommonInput
+          value={subject}
+          onChangeText={setSubject}
+          placeholder="Digite aqui"
+        />
+
         <Text>Mensagem</Text>
-          <MessageInput 
-            value={message}
-            onChangeText={setMessage}
-            placeholder="Digite aqui"
-            multiline
-            style={{ height: 100 }}
-          />
+        <MessageInput
+          value={message}
+          onChangeText={setMessage}
+          placeholder="Digite aqui"
+          multiline
+          style={{ height: 100 }}
+        />
 
         <Button variant="primary" title="Enviar" />
       </Container>
