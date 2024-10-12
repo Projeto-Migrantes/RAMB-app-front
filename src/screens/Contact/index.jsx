022782bbd1ae0413b4d885a24cc3a6406d7e2041
Pedro <pedrinhosousa.alves@gmail.com>
2024-10-12 18:00:43 -0300

    Atualização de estilização

--- src/screens/Contact/index.jsx
+++ src/screens/Contact/index.jsx
@@ -1,10 +1,10 @@
 import { Button } from "@components/Button";
 import { Header } from "@components/Header";
 import theme from "@theme/index";
-import { Text, View, TextInput } from "react-native";
+import { View, Text } from "react-native";
 import { TitleWithIcon } from "@components/TitleWithIcon";
 import AntDesign from "@expo/vector-icons/AntDesign";
-import { Container } from "./styles";
+import { Container, CommonInput, MessageInput} from "./styles";
 import { useState } from "react";
 
 export function Contact() {
@@ -13,7 +13,6 @@ export function Contact() {
   const [phone, setPhone] = useState("");
   const [subject, setSubject] = useState("");
   const [message, setMessage] = useState("");
-  
 
   return (
     <View style={{ flex: 1, backgroundColor: theme.Colors.White }}>
@@ -24,48 +23,48 @@ export function Contact() {
           <AntDesign name="contacts" size={26} color={theme.Colors.Purple} />
         }
       />
+      
       <Container>
-        <Text>Página de Entre em contato</Text>
+
         <Text>Nome</Text>
-        <TextInput
-        value={name}
-        onChangeText={setName}
-        placeholder="Digite aqui"
-        style={{ borderWidth: 1, borderColor: theme.Colors.Gray, padding: 10, marginBottom: 10 }}
-      />
-      <Text>Email</Text>
-        <TextInput
-          value={email}
-          onChangeText={setEmail}
-          placeholder="Digite aqui"
-          style={{ borderWidth: 1, borderColor: theme.Colors.Gray, padding: 10, marginBottom: 10 }}
-        />
+          <CommonInput
+            value={name}
+            onChangeText={setName}
+            placeholder="Digite aqui"
+          />
+
+        <Text>Email</Text>
+          <CommonInput
+            value={email}
+            onChangeText={setEmail}
+            placeholder="Digite aqui"
+          />
+
         <Text>Telefone</Text>
-        <TextInput
-        value={phone}
-        onChangeText={setPhone}
-        placeholder="Digite aqui"
-        style={{borderWidth:1, borderColor: theme.Colors.Gray, padding:10, marginBottom:10}}
-        />
+          <CommonInput
+            value={phone}
+            onChangeText={setPhone}
+            placeholder="Digite aqui"
+          />
+
         <Text>Assunto</Text>
-        <TextInput
-          value={subject}
-          onChangeText={setSubject}
-          placeholder="Digite aqui"
-          style={{ borderWidth: 1, borderColor: theme.Colors.Gray, padding: 10, marginBottom: 10 }}
-        />
+          <CommonInput
+            value={subject}
+            onChangeText={setSubject}
+            placeholder="Digite aqui"
+          />
+          
         <Text>Mensagem</Text>
-        <TextInput
-          value={message}
-          onChangeText={setMessage}
-          placeholder="Digite aqui"
-          multiline
-          style={{ borderWidth: 1, borderColor: theme.Colors.Gray, padding: 10, height: 100, marginBottom: 10 }}
-        />
+          <MessageInput 
+            value={message}
+            onChangeText={setMessage}
+            placeholder="Digite aqui"
+            multiline
+            style={{ height: 100 }}
+          />
 
         <Button variant="primary" title="Enviar" />
       </Container>
     </View>
-
   );
 }
--- src/screens/Contact/styles.js
+++ src/screens/Contact/styles.js
@@ -1,6 +1,26 @@
 import styled from "styled-components/native";
+import { TextInput } from "react-native";
 
 export const Container = styled.View`
-    align-items: center;
-    justify-content: center;
+  align-items: center;
+  justify-content: center;
+  flex: 1; 
+  justify-content: flex-start;
+  padding: 20px; /* Garante que o contêiner ocupe toda a tela */
+`;
+
+export const CommonInput = styled.TextInput`
+  width: 100%;
+  align-items: center;
+  border-width: 1px;
+  border-color: ${({ theme }) => theme.Colors.Blue};
+  border-radius: 999px; /* Bordas arredondadas */
+  padding: 5px 20px;
+  margin: 10px;
+  
+`;
+
+export const MessageInput = styled(CommonInput)`
+  padding: 20px; /* Espaçamento interno */
+  border-radius: 12px;
 `;
