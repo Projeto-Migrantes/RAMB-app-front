fe2c4ec980ad26f07e478e10eee54712dc95d1b8
Pedro <pedrinhosousa.alves@gmail.com>
2024-10-12 18:05:57 -0300

    atualizações de estilização

--- src/screens/Contact/styles.js
+++ src/screens/Contact/styles.js
@@ -6,7 +6,7 @@ export const Container = styled.View`
   justify-content: center;
   flex: 1; 
   justify-content: flex-start;
-  padding: 20px; /* Garante que o contêiner ocupe toda a tela */
+  padding: 20px;
 `;
 
 export const CommonInput = styled.TextInput`
@@ -14,13 +14,13 @@ export const CommonInput = styled.TextInput`
   align-items: center;
   border-width: 1px;
   border-color: ${({ theme }) => theme.Colors.Blue};
-  border-radius: 999px; /* Bordas arredondadas */
+  border-radius: 999px; 
   padding: 5px 20px;
   margin: 10px;
   
 `;
 
 export const MessageInput = styled(CommonInput)`
-  padding: 20px; /* Espaçamento interno */
+  padding: 20px;
   border-radius: 12px;
 `;
