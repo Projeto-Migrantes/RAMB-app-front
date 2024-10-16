b051bcd945397e1dfec97efa909e827455815900
Beatriz Siquara <beatrizsiquara@gmail.com>
2024-10-16 16:04:44 -0300

    Feat [Animation in Titlte]

--- src/screens/ChangeLanguage/index.jsx
+++ src/screens/ChangeLanguage/index.jsx
@@ -1,25 +1,41 @@
 import { Header } from "@components/Header";
-import { Container, Title } from "./styles";
+import { Container, Title, TypeWriterStyled } from "./styles";
 import { Button } from "@components/Button";
 import { useNavigation } from "@react-navigation/native";
 import { View } from "react-native";
 import theme from "@theme/index";
 import { TitleWithDescription } from "@components/TitleWithDescription";
-
+import TypeWriter from "react-native-typewriter";
+import { useEffect, useState } from "react";
 export function ChangeLanguage() {
   const navigation = useNavigation();
 
   function handleChangeScreen() {
     navigation.navigate("login");
   }
+  const texts = [
+    "Escolha seu Idioma",
+    "Choose your Language",
+    "Choisissez votre Langue",
+    "Elige tu Idioma",
+  ];
 
+  const [currentTextIndex, setCurrentTextIndex] = useState(0);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setCurrentTextIndex((prevIndex) => (prevIndex + 1) % texts.length);
+    }, 6000); 
+
+    return () => clearTimeout(timer);
+  }, [currentTextIndex]);
   return (
     <View style={{ flex: 1, backgroundColor: theme.Colors.White }}>
       <Header />
       <Container>
-        <TitleWithDescription
-          title="Escolha o seu Idioma:"
-        />
+        <TypeWriterStyled typing={1} minDelay={100} maxDelay={100}>
+          {texts[currentTextIndex]}
+        </TypeWriterStyled>
         <Button
           variant="primary"
           title="Escolher"
