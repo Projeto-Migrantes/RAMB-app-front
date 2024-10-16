62260c6c936678181adb713f654816a840977e03
guilherme théo ribeiro rêgo <guilhermetheoribeiror@gmail.com>
2024-10-16 16:24:09 -0300

    Novamente espero que esteja tudo certo

--- src/components/ButtonLanguage/index.jsx
+++ src/components/ButtonLanguage/index.jsx
@@ -1,8 +1,9 @@
 import { Container, Title } from "./styles";
 
-export function ButtonLanguage({title, ...props }) {
+export function ButtonLanguage({title,imgSrc, ...props }) {
   return (
     <Container{...props}>
+    <img src={imgSrc} alt={altText} width={24} height={24} />
       <Title>{title}</Title>
     </Container>
   );
--- src/screens/ChangeLanguage/index.jsx
+++ src/screens/ChangeLanguage/index.jsx
@@ -1,11 +1,15 @@
 import { Header } from "@components/Header";
 import { Container, Title } from "./styles";
 import { Button } from "@components/Button";
+import { ButtonLanguage } from "@components/Button";
 import { useNavigation } from "@react-navigation/native";
 import { View } from "react-native";
 import theme from "@theme/index";
 import { TitleWithDescription } from "@components/TitleWithDescription";
-
+import FlagBrazil from "@assets/BrazilFlag.svg";
+import FlagSpain from "@assets/SpainFlag.svg";
+import FlagUsa from "@assets/UsaFlag.svg";
+import FlagFrance from "@assets/FranceFlag.svg";
 export function ChangeLanguage() {
   const navigation = useNavigation();
 
@@ -20,8 +24,29 @@ export function ChangeLanguage() {
         <TitleWithDescription
           title="Escolha o seu Idioma:"
         />
+        <ButtonLanguage
+          title="Português" 
+          onPress={handleChangeScreen}
+          imgSrc={FlagBrazil}
+        />
+        <ButtonLanguage
+          title="English" 
+          onPress={handleChangeScreen}
+          imgSrc={FlagUsa}
+        />
+           <ButtonLanguage
+          title="Français" 
+          onPress={handleChangeScreen}
+          imgSrc={FlagFrance}
+        />
+          <ButtonLanguage
+          title="Español" 
+          onPress={handleChangeScreen}
+          imgSrc={FlagSpain}
+          />
+         
         <Button
-          variant="primary"
+          
           title="Escolher"
           onPress={handleChangeScreen}
         />
