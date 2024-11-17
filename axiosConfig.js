a698150e2744cfc0eb26086bc2c173457a45c581
Alisson Oliveira <1272322139@ulife.com.br>
2024-11-16 22:53:57 -0300

    Atualização na configuração do Axios

--- axiosConfig.js
+++ axiosConfig.js
@@ -2,10 +2,14 @@ import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import jwtDecode from 'jwt-decode';
 import { useNavigation } from '@react-navigation/native';
+import { API_URL, SECRET_KEY } from '@env';
 
 const api = axios.create({
-  baseURL: 'https://api-ramb.up.railway.app/api/v1', 
-  headers: { 'Content-Type': 'application/json' }
+  headers: {
+    'Content-Type': 'application/json',
+    'x-private-key': SECRET_KEY
+  },
+  baseURL: API_URL
 });
 
 
